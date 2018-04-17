670835e3bdbbc6676ec9b255cb147cfcc5acbca6
sharon <sharonoptsout@gmail.com>
2018-04-17 22:40:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,3 +3,8 @@ var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 function addElementToBeginningOfArray(anar,anel) {
   return([anel,...anar]);
 }
+
+function destructivelyAddElementToBeginningOfArray(anar,anel) {
+  var anar[0] = anel;
+  return(anar);
+}
