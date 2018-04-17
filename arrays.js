c20abfd23c3ae2d1f4c03ae1f8f5a3de81544bbf
sharon <sharonoptsout@gmail.com>
2018-04-17 22:06:59 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,11 @@ var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
 function addElementToBeginningOfArray() {
   var myArray = [1,2,3];
   var myArray2 = [4,...myArray];
-  return (myArray2);
+  return(myArray2);
 }
 
-function destructivelyAddElementToBeginningOfArray() {}
+function destructivelyAddElementToBeginningOfArray() {
+  var myArray3 = [1,2,3];
+  var myArray3[0] = 5;
+  return(myArray3);
+}
