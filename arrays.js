bc12278b26fd0954edb3724968c6a35c65c6c0d1
sharon <sharonoptsout@gmail.com>
2018-04-17 22:49:19 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,32 @@
+var chocolateBars = ['snickers','hundred grand','kitkat','skittles'];
+
+function addElementToBeginningOfArray(anar,anel) {
+  return([anel,...anar]);
+}
+///////////////
+function destructivelyAddElementToBeginningOfArray(anar,anel) {
+  var anar[0] = anel;
+  return(anar);
+}
+//////////////////
+
+function accessElementInArray(anarray,ind) {
+  return(anarray[ind]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(anotherarray) {
+  return(anotherarray.shift());
+}
+
+function removeElementFromBeginningOfArray(somearray) {
+  return(somearray.slice(1));
+}
+
+function destructivelyRemoveElementFromEndOfArray(thingy) {
+  thingy.pop();
+  return(thingy);
+}
+
 function removeElementFromEndOfArray(th) {
   return(th.slice(0,th.length-1));
 }
